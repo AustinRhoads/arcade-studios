b414ccd6ebbe7b2a42334b916176980f2f3d77be
Austin Rhoads <austinrrhoads@ymail.com>
2021-03-05 12:33:04 -0600

    added controller logic

--- frontend/src/controller.js
+++ frontend/src/controller.js
@@ -1,21 +1,45 @@
-const Controller = function(){
-
-    this.left = new Controller.ButtonInput();
-    this.up = new Controller.ButtonInput();
-    this.right = new Controller.ButtonInput();
+//more advanced game controller come back later homey
 
 
-    this.keyDownUp = function(type, key_code){
-        var isKeyDown = (type == "keyDown")? true:false;
+//const Controller = function(){
+//
+//    this.left = new Controller.ButtonInput();
+//    this.up = new Controller.ButtonInput();
+//    this.right = new Controller.ButtonInput();
+//
+//
+//    this.keyDownUp = function(type, key_code){
+//        var isKeyDown = (type == "keyDown")? true:false;
+//
+//        switch(key_code){
+//            case 37: this.left.getInput(isKeyDown); 
+//            console.log("left" + `${isKeyDown}`);
+//            break;
+//            case 38: this.up.getInput(isKeyDown);
+//            console.log("up" + `${isKeyDown}`);
+//            break;
+//            case 39: this.right.getInput(isKeyDown);
+//            console.log("down" + `${isKeyDown}`);
+//        };
+//    };
+//
+//};
+//
+//Controller.prototype = {
+//    contructor : Controller
+//};
+//
+//
+//Controller.ButtonInput = function() {
+//    this.active = this.isKeyDown = false;
+//}
+//
+//Controller.ButtonInput.prototype = {
+//    constructor : Controller.ButtonInput,
+//    getInput : function(isKeyDown) {
+//        if (this.isKeyDown != isKeyDown) this.active = isKeyDown;
+//        this.isKeyDown = isKeyDown;
+//    }
+//}
 
-        switch(key_code){
-            case 37: this.left.getInput(isKeyDown); 
-            break;
-            case 38: this.up.getInput(isKeyDown);
-            break;
-            case 39: this.right.getInput(isKeyDown);
-        };
-    };
-
-};
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -75,3 +75,77 @@ function loadGameObject(obj){
 
   console.log(obj);
 }
+
+
+
+
+
+
+
+
+    /////////////////////////
+   //// SIMPLY GAME BUILD///
+  /////////////////////////
+  
+  var context, controller, rectangle, loop;
+
+  context = document.querySelector("canvas").getContext("2d");
+
+  context.canvas.height = 180; //replace with game.canvas_height attr
+  context.canvas.width = 320; //replace with game.canvas_width attr
+
+  rectangle = {
+    height: 32,
+    width: 32,
+    jumping: true,
+    x: 144, //center of canvas spawn placement
+    y: 0,
+    x_velocity: 0,
+    y_velocity: 0
+  };
+
+
+
+   //////////////////////
+  ///CONTROLLER LOGIC///
+ //////////////////////
+
+
+ controller = {
+   left: false,
+   right: false,
+   up: false,
+
+  keyListener: function(event) {
+    var key_state = (event.type == "keydown")? true:false;
+    switch(event.keyCode){
+      case 37: //left
+        controller.left = key_state;
+        console.log("left" + `${key_state}`);
+      break;
+      case 38: //up
+        controller.up = key_state;
+        console.log("up" + `${key_state}`);
+      break;
+      case 39: //right
+        controller.right = key_state;
+        console.log("right" + `${key_state}`);
+      break;
+    }
+  }
+
+ }
+
+
+
+ //move next two lines to bottom for cleanliness, "cleanliness is next to jimi hendrixliness"
+ window.addEventListener("keydown", controller.keyListener)
+ window.addEventListener("keyup", controller.keyListener)
+
+
+
+
+  //////////////////////
+ ///GAME ENGINE LOOP///
+//////////////////////
+ 
