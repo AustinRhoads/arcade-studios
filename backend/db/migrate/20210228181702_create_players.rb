86f1181e4809d4a2753a1ad2041df77c44983806
Austin Rhoads <austinrrhoads@ymail.com>
2021-03-05 15:00:29 -0600

    added height, width velocities and jumping boolean to player, some to baddies and game

--- backend/db/migrate/20210228181702_create_players.rb
+++ backend/db/migrate/20210228181702_create_players.rb
@@ -2,8 +2,14 @@ class CreatePlayers < ActiveRecord::Migration[6.0]
   def change
     create_table :players do |t|
       t.string :name
+      t.integer :height, :default => 32
+      t.integer :width, :default => 32
       t.float :speed, :default => 0.5
-      t.integer :jumping_height, :default => 25
+      t.integer :jumping_height, :default => 20
+      t.boolean :jumping, :default => true
+      t.integer :x_velocity, :default => 0
+      t.integer :y_velocity, :default => 0
+
 
       t.references :game, foreign_key: true
       t.timestamps
--- backend/db/migrate/20210303181826_create_baddies.rb
+++ backend/db/migrate/20210303181826_create_baddies.rb
@@ -2,7 +2,13 @@ class CreateBaddies < ActiveRecord::Migration[6.0]
   def change
     create_table :baddies do |t|
       t.string :name
-      t.integer :speed
+      t.float :speed, :default => 0.5
+      t.integer :height, :default => 32
+      t.integer :width, :default => 32
+      t.float :speed, :default => 0.5
+      t.integer :x_velocity, :default => 0
+      t.integer :y_velocity, :default => 0
+
 
       t.references :game, foreign_key: true
       t.timestamps
