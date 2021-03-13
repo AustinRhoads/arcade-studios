9fd515133d101c11776aad9cb6b7ecaa0f9c0c1d
Austin Rhoads <austinrrhoads@ymail.com>
2021-03-12 19:43:04 -0600

    update default

--- backend/db/migrate/20210303153108_create_games.rb
+++ backend/db/migrate/20210303153108_create_games.rb
@@ -4,8 +4,8 @@ class CreateGames < ActiveRecord::Migration[6.0]
       t.string :name
       t.float :gravity, :default => 1.5
       t.float :friction, :default => 0.9
-      t.integer :canvas_width, :default => 1000
-      t.integer :canvas_height, :default => 550
+      t.integer :canvas_width, :default => 960
+      t.integer :canvas_height, :default => 560
       t.timestamps
     end
   end
